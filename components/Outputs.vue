e74cb114969170268e70e256c5f6e185c4d10c78
Thomas T <huulbaek@gmail.com>
2023-10-26 22:27:39 +0200

    Table

--- components/Outputs.vue
+++ components/Outputs.vue
@@ -5,6 +5,28 @@
       <div class="flex-1">{{ key }}</div>
       <div>{{ value }}</div>
     </div>
+    <table v-if="result.spaces" class="w-full min-w-full divide-y divide-gray-300">
+      <thead>
+        <tr class="divide-x divide-gray-200">
+          <th>Space</th>
+          <th>Area</th>
+          <th>Space</th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200 bg-white">
+        <template v-for="(value, key) in result.spaces">
+          <tr class="divide-x divide-gray-200">
+            <td :rowspan="value.spaces ? value.spaces.length + 1 : 1" class="-rotate-90 text-center whitespace-nowrap h-10">{{ value.name }}</td>
+            <td :rowspan="value.spaces ? value.spaces.length + 1 : 1">{{ value.result.adjustedAreaInclCompensationWithAdjustmentAndCompensation }}</td>
+          </tr>
+          <template v-for="(value2, key2) in value.spaces">
+            <tr class="divide-x divide-gray-200">
+              <td>{{ value2.name }}</td>
+            </tr>
+          </template>
+        </template>
+      </tbody>
+    </table>
   </div>
 </template>
 
